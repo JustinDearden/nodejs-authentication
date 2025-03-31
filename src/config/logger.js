fadc004dd2855445a8207c6823402059d5a7a581
Justin Dearden <deardenj@protonmail.com>
2025-03-30 22:10:14 -0400

    New Lines

--- src/config/logger.js
+++ src/config/logger.js
@@ -1,37 +1,32 @@
-const fs = require('fs');
-const path = require('path');
-const winston = require('winston');
+const fs = require("fs");
+const path = require("path");
+const winston = require("winston");
 const { combine, timestamp, printf, colorize } = winston.format;
 
 // Resolve logs directory relative to project root
-const logDir = path.join(__dirname, '../../logs');
+const logDir = path.join(__dirname, "../../logs");
 if (!fs.existsSync(logDir)) {
-    fs.mkdirSync(logDir);
+  fs.mkdirSync(logDir);
 }
 
 const myFormat = printf(({ level, message, timestamp }) => {
-    return `${timestamp} ${level}: ${message}`;
+  return `${timestamp} ${level}: ${message}`;
 });
 
 const logger = winston.createLogger({
-    level: 'info',
-    format: combine(
-        timestamp(),
-        myFormat
-    ),
-    transports: [
-        new winston.transports.Console({
-            format: combine(
-                colorize(),
-                timestamp(),
-                myFormat
-            )
-        }),
-        new winston.transports.File({ filename: path.join(logDir, 'app.log') })
-    ],
-    exceptionHandlers: [
-        new winston.transports.File({ filename: path.join(logDir, 'exceptions.log') })
-    ]
+  level: "info",
+  format: combine(timestamp(), myFormat),
+  transports: [
+    new winston.transports.Console({
+      format: combine(colorize(), timestamp(), myFormat),
+    }),
+    new winston.transports.File({ filename: path.join(logDir, "app.log") }),
+  ],
+  exceptionHandlers: [
+    new winston.transports.File({
+      filename: path.join(logDir, "exceptions.log"),
+    }),
+  ],
 });
 
 module.exports = logger;
