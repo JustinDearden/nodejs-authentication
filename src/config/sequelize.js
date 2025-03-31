29481aca1d02b5dad1b3b8e735fca9b097656d7b
Justin Dearden <deardenj@protonmail.com>
2025-03-30 22:07:36 -0400

    Sequelize Setup

--- src/config/sequelize.js
+++ src/config/sequelize.js
@@ -1,15 +1,21 @@
-const { Sequelize } = require('sequelize');
+const { Sequelize } = require("sequelize");
 
 const sequelize = new Sequelize(
-    process.env.PGDATABASE || 'authdb',
-    process.env.PGUSER || 'postgres',
-    process.env.PGPASSWORD || 'postgres',
-    {
-        host: process.env.PGHOST || 'localhost',
-        port: process.env.PGPORT || 5432,
-        dialect: 'postgres',
-        logging: false
-    }
+  process.env.PGDATABASE || "authdb",
+  process.env.PGUSER || "postgres",
+  process.env.PGPASSWORD || "postgres",
+  {
+    host: process.env.PGHOST || "localhost",
+    port: process.env.PGPORT || 5432,
+    dialect: "postgres",
+    logging: process.env.SEQUELIZE_LOGGING === "true" ? console.log : false,
+    pool: {
+      max: parseInt(process.env.PGPOOL_MAX) || 10,
+      min: parseInt(process.env.PGPOOL_MIN) || 0,
+      acquire: parseInt(process.env.PGPOOL_ACQUIRE) || 30000,
+      idle: parseInt(process.env.PGPOOL_IDLE) || 10000,
+    },
+  }
 );
 
 module.exports = sequelize;
